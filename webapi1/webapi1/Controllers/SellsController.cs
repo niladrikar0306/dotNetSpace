85bfcfd4a2d0456f3584916f1d7a0beeddaafbba
niladrikar0306 <niladri.kar0306@gmail.com>
2017-07-30 17:43:42 +0530

    webapi1

---
+++ webapi1/webapi1/Controllers/SellsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webapi1.beans;
+
+namespace webapi1.Controllers
+{
+    public class SellsController : ApiController
+    {
+        public IEnumerable<Sells> Get()
+        {
+            var li = new List<Sells>();
+
+            Sells s1 = new Sells();
+            s1.CustomerId = "1";
+            s1.ProductName = "p1";
+            li.Add(s1);
+
+
+            return li;
+        }
+    }
+}
---
+++ webapi1/webapi1/beans/Sells.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webapi1.beans
+{
+    public class Sells
+    {
+        public String CustomerId { get; set; }
+        public String ProductName { get; set; }
+    }
+}
